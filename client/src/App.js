472cc8f401b48507f6aa225fc27456549c36dd52
Omar Faruque <omar.faruque967@gmail.com>
2019-02-18 01:42:10 +0600

    some changes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -19,66 +19,110 @@ import { Provider } from './components/Context';
 import './App.css';
 
 class App extends Component {
-
   state = {
-    user: ''
+    user: "",
+    emailAddress: "",
+    password: "",
+    courses: [],
+    currentlySignedIn: false,
+    validationError: false,
+    validationMessage: "",
+    emailFormatError: "",
+    emailMissing: "",
+    passwordMissing: "",
+    loginError: "",
+    redirectToPreviousRoute: false
   }
 
-  // authentication
-  signIn = (email, pass, history) => {
+  signIn = (emailAddress, password) => {
     axios.get('http://localhost:5000/api/users', {
-      auth: { username: email, password: pass }
+      auth: { username: emailAddress, password: password }
     })
       .then(res => {
-        if (res.status === 200) {
-          localStorage.setItem('idUserLogin', res.data._id);
-          localStorage.setItem('firstName', res.data.firstName);
-          localStorage.setItem('lastName', res.data.lastName);
-          localStorage.setItem('password', res.data.password);
-          localStorage.setItem('emailAddress', res.data.emailAddress);
-
-          const firstName = localStorage.getItem('firstName');
-          const lastName = localStorage.getItem('lastName');
-          const password = localStorage.getItem('password');
-          const emailAddress = localStorage.getItem('emailAddress');
-          const idUserLogin = localStorage.getItem('idUserLogin');
+        if (res.status === 200 || res.status === 304) {
           this.setState({
-            user: { firstName, lastName, password, emailAddress, idUserLogin }
+            user: res.data,
+            currentlySignedIn: true,
+            validationError: false,
+            validationMessage: ""
           });
-          history.goBack();
+
+          localStorage.setItem("user", JSON.stringify(res.data));
+          localStorage.setItem("auth", JSON.stringify(res.config.headers.Authorization));
         }
       })
-      .catch(err => console.log(err));
+      .catch(error => {
+        if (error.res.status === 400) {
+          this.setState({ currentlySignedIn: false, validationError: true, validationMessage: "Validation Error" })
+          if (error.res.data.message === "Email must be correctly formatted (name@example.com)") {
+            this.setState({ emailFormatError: "Email must be correctly formatted (name@example.com)" })
+          }
+          if (error.res.data.message === "Email address is required") {
+            this.setState({ emailMissing: "Email address is required" })
+          }
+          if (error.res.data.message === "Password is required") {
+            this.setState({ passwordMissing: "Password is required" })
+          }
+        } else if (error.res.status === 401) {
+          this.setState({ validationError: true, validationMessage: "Error", loginError: error.res.data.message });
+        } else if (error.res.status === 500) {
+          this.state.history.push('/error');
+        }
+      });
+  }
+
+
+  signOut = () => {
+    // Removing the email and pass from the global state
+    this.setState({
+      user: "",
+      currentlySignedIn: false,
+      validationError: false
+    });
+    localStorage.clear();
+  }
+
+
+  componentDidMount() {
+    // Be logged in
+    if (localStorage.user) {
+      let user = JSON.parse(window.localStorage.getItem('user'));
+      this.signIn(user.emailAddress, user.password)
+    }
   }
 
   render() {
-
-    const firstName = localStorage.getItem('firstName');
-    const lastName = localStorage.getItem('lastName');
-    const password = localStorage.getItem('password');
-    const emailAddress = localStorage.getItem('emailAddress');
-    const idUserLogin = localStorage.getItem('idUserLogin');
-    const user = { firstName, lastName, password, emailAddress, idUserLogin }
-    const loggedIn = emailAddress;
-
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Route path="/" render={props => <Header user={user} />} />
-          <Switch>
-            <Route exact path="/" render={props => <Courses user={user} />} />
-            <Route exact path="/courses/create" render={props => <CreateCourse user={user} />} />
-            <Route path="/courses/:id/update" render={props => <UpdateCourse />} />
-            <Route exact path="/courses/:id" render={props => <CourseDetail id={props.match.params.id} user={user} />} />
-            <Route path="/signin" render={props => <UserSignIn signIn={this.signIn} history={props.path} />} />
-            <Route path="/signup" render={() => loggedIn ? (<Redirect to="/" />) : (<UserSignUp />)} />
-            <Route path="/signout" render={props => <UserSignOut />} />
-            <Route path="/forbidden" render={props => <Forbidden />} />
-            <Route path="/error" render={props => <Error />} />
-            <Route component={NotFound} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <Provider
+        value={{
+          user: this.state.user,
+          currentlySignedIn: this.state.currentlySignedIn,
+          validationError: this.state.userValidationError,
+          validationMessage: this.state.validationMessage,
+          loginError: this.state.loginError,
+          actions: { signIn: this.signIn, signOut: this.signOut }
+        }}
+      >
+        <BrowserRouter>
+          <div>
+            <Header currentlySignedIn={this.state.currentlySignedIn} signOut={this.signOut} />
+            <Switch>
+              <Route exact path='/' render={() => <Redirect to='/courses' />} />
+              <Route exact path='/courses' component={Courses} />
+              <PrivateRouteCreate exact path='/courses/create' component={CreateCourse} user={this.state.user} />
+              <PrivateRouteUpdate exact path='/courses/:id/update' component={UpdateCourse} user={this.state.user} />
+              <Route exact path='/courses/:id' component={CourseDetail} user={this.state.user} />
+              <Route exact path='/signin' render={() => <UserSignIn signIn={this.signIn} validationError={this.state.validationError} validationMessage={this.state.validationMessage} emailFormatError={this.state.emailFormatError} emailMissing={this.state.emailMissing} passwordMissing={this.state.passwordMissing} loginError={this.state.loginError} />} />
+              <Route exact path='/signup' render={() => <UserSignUp signIn={this.signIn} />} />
+              <Route exact path='/signout' render={() => <UserSignOut signOut={this.signOut} />} />
+              <Route exact path='/notfound' component={NotFound} />
+              <Route exact path='/forbidden' component={Forbidden} />
+              <Route exact path='/error' component={Error} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }

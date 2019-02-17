a56b4c096e45a2dbea4abf7ada10f5703c3b369a
Omar Faruque <omar.faruque967@gmail.com>
2019-02-17 23:29:51 +0600

    change db name

--- api/app.js
+++ api/app.js
@@ -18,7 +18,7 @@ app.use(cors());
 
 // mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/restapi', {
+mongoose.connect('mongodb://localhost:27017/fsjstd-restapi', {
   useCreateIndex: true,
   useNewUrlParser: true
 });
